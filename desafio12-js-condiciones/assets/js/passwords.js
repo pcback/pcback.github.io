9bda7947c988c192996aaa53fe3712baeb74a464
pcback <pcbackrmartinez@gmail.com>
2023-10-25 02:04:01 -0300

    add: arch_estilo_passwords.js  desafio_12

---
+++ desafio12-js-condiciones/assets/js/passwords.js
@@ -0,0 +1,5 @@
+// se crean las const en el archivo javascript
+const select1 = document.querySelector("#valor1"); //se crea la const select y se asigna al dom el valor del id del elemento html
+const select2 = document.querySelector("#valor2");
+const select3 = document.querySelector("#valor3");
+const elementoPararfo = document.querySelector("#mensaje"); //se crea la const parrafo y se asigna al dom el valor del id del elemento html
