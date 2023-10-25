7a6e23e8a3a76127f5f09196b6b90e01e96ec1e5
pcback <pcbackrmartinez@gmail.com>
2023-10-25 02:04:22 -0300

    add: arch_estilo_stickers.js  desafio_12

---
+++ desafio12-js-condiciones/assets/js/stcker.js
@@ -0,0 +1,4 @@
+const valor1 = document.querySelector("#cantidad1"); //se crea la const valor y se asigna al dom el valor del id del elemento html
+const valor2 = document.querySelector("#cantidad2");
+const valor3 = document.querySelector("#cantidad3");
+const msje1 = document.querySelector("#mensaje");
