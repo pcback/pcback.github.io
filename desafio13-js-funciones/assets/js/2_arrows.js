25f3fc27761809bea0ea27fdb8e2c901532ad95c
pcback <pcbackrmartinez@gmail.com>
2023-10-25 02:14:27 -0300

    add: arch_arrows.js  desafio_13

---
+++ desafio13-js-funciones/assets/js/2_arrows.js
@@ -0,0 +1,7 @@
+//////////////////////////
+let suma = (a, b) => a + b;
+var resultado = suma(1, 2);
+
+const mensaje = document.querySelector("#parrafo");
+
+mensaje.textContent = "El resultado es  " + resultado;
