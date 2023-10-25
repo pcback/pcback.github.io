ea685fc281c672c0567a2b0f8b6d51b68c4d055b
pcback <pcbackrmartinez@gmail.com>
2023-10-25 02:03:36 -0300

    add: arch_estilo_imagen.js  desafio_12

---
+++ desafio12-js-condiciones/assets/js/imagen.js
@@ -0,0 +1,3 @@
+// se crean las const en el archivo javascript
+const elementoImagen1 = document.querySelector("#imagen1");
+//se crea la const elementoimagen1 y se asigna al dom el valor del id del elemento html
