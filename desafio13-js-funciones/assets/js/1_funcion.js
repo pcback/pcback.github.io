37252548b8432fc4ecb126c6c64c53abc4fe5383
pcback <pcbackrmartinez@gmail.com>
2023-10-25 02:14:04 -0300

    add: arch_funcion.js  desafio_13

---
+++ desafio13-js-funciones/assets/js/1_funcion.js
@@ -0,0 +1,13 @@
+// FUNCION DE EXPRESION
+
+example = function (a, b, c) {
+  return a + b + c;
+};
+
+var resultado = example(1, 2, 3);
+
+const mensaje = document.querySelector("#parrafo");
+
+mensaje.textContent = "El resultado es  " + resultado;
+
+////////////////////////
